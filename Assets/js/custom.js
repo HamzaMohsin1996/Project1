ca885f89dd44bad54a1782379057c1fd636de3f3
“HamzaMohsin1996” <hamza95.hw@gmail.com>
2023-11-23 00:23:17 +0500

    Revert "first commit"
    This reverts commit deca4f6d8fe378f2797053b9c9a1d5cbbd484d5c.

--- Assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-jQuery.noConflict();
-(function( $ ) {
-  $(function() {
-    // More code using $ as alias to jQuery
-    $('button').click(function(){
-        $('#exampleModal').modal('show');
-    });
-  });
-})(jQuery);
